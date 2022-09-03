880ec057e097894e7bb909370f585a330b7a9b00
Edward <edwardxiao29@gmail.com>
2022-09-03 14:07:57 -0400

    add csrf trusted

--- promProject/settings.py
+++ promProject/settings.py
@@ -22,11 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'nwprom.herokuapp.com', 'nwprom.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['https://nwprom.up.railway.app','https://*.127.0.0.1']
 
 
 # Application definition
